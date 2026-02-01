e62d9686d9f3437a640f1c4ae996fac53520b67b
khushicomp <khushiyuwanathe8@gmail.com>
2026-02-01 15:59:59 +0530

    Use Environment Port for deployment

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/", indexRouter);
 
 app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
